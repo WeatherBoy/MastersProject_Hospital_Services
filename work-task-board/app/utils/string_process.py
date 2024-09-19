092abaee9f67abedb58096bd24b1869f54fa063d
WeatherBoy <dafloxi@gmail.com>
2024-09-19 16:13:06 +0200

    :sparkles: \utils\string_process.py - add `soup_to_weekly_taskboards( )`

--- work-task-board/app/utils/string_process.py
+++ work-task-board/app/utils/string_process.py
@@ -1,4 +1,6 @@
 import re
+import bs4
+from TaskBoard import TaskBoard, FunctionAssignment
 
 
 def empty_cell(cell_value: str) -> bool:
@@ -54,3 +56,58 @@ def regex_formatting_time_name(cell_split: str) -> tuple[str, dict[str, str]] | 
         # NOTE: This is poor error handling - placeholder
         # Maybe I have decided, that this is adequate error handling
         return None, None
+
+
+def soup_to_weekly_taskboards(soup: bs4.BeautifulSoup, skipable_funcs: list[int], num_weekdays: int = 7) -> list[TaskBoard]:
+    """
+    This utilizes the `TaskBoard` and `FunctionAssignment` classes to create a list of `TaskBoard` objects, based on the parsed HTML content.
+    This serves to yield a representation of the task board for each day of the week.
+
+    :param soup: The parsed HTML content.
+    :param skipable_funcs: A list of function indices that should be skipped.
+    :param num_weekdays: The number of weekdays in the schedule. Default is 7.
+
+    :return: A list of `TaskBoard` objects, each representing a day of the week.
+    """
+    days = [None] * num_weekdays
+
+    functions = soup.find_all("div", class_="single-function")
+    data = soup.find_all("div", class_="single-description")
+
+    for indx, cell in enumerate(data):
+        if empty_cell(cell.text):
+            continue
+
+        day_indx = indx % num_weekdays
+        function_indx = indx // num_weekdays
+
+        if function_indx in skipable_funcs:
+            # Currently I skip 'Ergo aktiviteter' as, as far as I can see, they seem to be an outlier.
+            # Also, I skip 'Børn syg og ferie' as they are not relevant for the task board.
+            # NOTE: Mention this for nurse.
+            continue
+
+        cell_splits = cell.text.split("\n")
+        for cell_split in cell_splits:
+            if empty_cell(cell_split):
+                continue
+
+            name_formatted, data_formatted = regex_formatting_time_name(cell_split)
+            if format_name is None or data_formatted is None:
+                raise Exception(
+                    "If `regex_formatting_time_name( )` returns None, then there is no match for the regex pattern. And you should update approach."
+                )
+
+            function_assignment = FunctionAssignment(
+                function_name=functions[function_indx].text,
+                time=data_formatted["Time"],
+                extras=data_formatted["Extra"],
+            )
+
+            if days[day_indx] is None:
+                taskboard = TaskBoard()
+                days[day_indx] = taskboard
+
+            days[day_indx].add_function_to_nurse(name_formatted, function_assignment)
+
+    return days
