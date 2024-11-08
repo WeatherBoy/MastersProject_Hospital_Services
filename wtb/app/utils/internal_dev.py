f12dbe77cd9d427aa313ac0593e52c7bb5f9f153
WeatherBoy <dafloxi@gmail.com>
2024-11-08 16:46:24 +0100

    :technologist: add `save_functions_mismatch( )`
    I think the description and name of the function are pretty self explanatory.

--- wtb/app/utils/internal_dev.py
+++ wtb/app/utils/internal_dev.py
@@ -1,4 +1,9 @@
+import datetime
+
+import pandas as pd
+
 from app.data_structures.taskboard import TaskBoard
+from app.utils.os_structure import get_week_dates_from_today
 
 
 def print_taskboards(weekly_taskboards: list[TaskBoard], config: dict[str, any] = None) -> None:
@@ -21,3 +26,49 @@ def print_taskboards(weekly_taskboards: list[TaskBoard], config: dict[str, any] 
             else:
                 print(f"{weekdays[indx]}, TaskBoard:")
             print(taskboard.to_dataframe())
+
+
+def save_functions_mismatch(weekly_taskboards: list[TaskBoard], non_matching_functions: list[list[str]]) -> None:
+    """
+    Meant only for internal development.
+
+    Saves the mismatching functions between the HosInfo and Stuefordeling task boards to an Excel file.
+
+    :param weekly_taskboards: A list of TaskBoard objects. Ordered by day of the week.
+    :param non_matching_functions: A list of lists of strings. Each list contains the mismatching functions for a day.
+    """
+    today = datetime.date.today()
+    year, week, weekday = today.isocalendar()
+
+    filename = f"data/functions_mismatch/{year}_Week_{week}.xlsx"
+
+    week_dates = get_week_dates_from_today(today, weekday)
+    dfs = []
+
+    col1_id = "Alle funktioner HosInfo"
+    col2_id = "Stuefordeling funktioner uden par"
+
+    for indx, taskboard in enumerate(weekly_taskboards):
+        if taskboard is not None:
+            taskboard_funcs = taskboard.get_function_names()
+            taskboard_funcs.sort(key=str.lower)
+            stuefordeling_funcs = non_matching_functions[indx]
+
+            if stuefordeling_funcs is not None:
+                df = []
+
+                stuefordeling_funcs.sort(key=str.lower)
+
+                for i in range(len(taskboard_funcs)):
+                    if i < len(stuefordeling_funcs):
+                        df.append({col1_id: taskboard_funcs[i], col2_id: stuefordeling_funcs[i]})
+                    else:
+                        df.append({col1_id: taskboard_funcs[i], col2_id: None})
+
+            dfs.append(pd.DataFrame(df))
+
+    # Write to Excel
+    with pd.ExcelWriter(filename, engine="openpyxl") as writer:
+        for i, df in enumerate(dfs):
+            name = str(week_dates[i])
+            df.to_excel(writer, sheet_name=name, index=False)
