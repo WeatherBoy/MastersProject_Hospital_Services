bab54e6d7cf36697cc6a9e1940f14168b3508634
WeatherBoy <dafloxi@gmail.com>
2024-11-12 14:53:53 +0100

    :sparkles: update main.py with new functionality

--- wtb/app/main.py
+++ wtb/app/main.py
@@ -1,7 +1,7 @@
 import toml
 
-from app.utils.data_formatting import soup_to_weekly_taskboards
-from app.utils.internal_dev import print_taskboards
+from app.utils.data_formatting import soup_to_weekly_taskboards, update_taskboards_with_stuefordeling
+from app.utils.internal_dev import save_functions_mismatch
 from app.utils.os_structure import save_weekly_taskboards
 from app.utils.webscrape import get_soup_from_altiplan
 
@@ -13,5 +13,7 @@ if __name__ == "__main__":
     soup = get_soup_from_altiplan(config)
 
     weekly_taskboards = soup_to_weekly_taskboards(soup, config)
-    print_taskboards(weekly_taskboards, config)
-    save_weekly_taskboards(weekly_taskboards, NUM_WEEKDAYS)
+    updated_weekly_taskboards, non_matching_functions = update_taskboards_with_stuefordeling(weekly_taskboards)
+    # print_taskboards(updated_weekly_taskboards, config)
+    save_functions_mismatch(weekly_taskboards, non_matching_functions)
+    save_weekly_taskboards(updated_weekly_taskboards, NUM_WEEKDAYS)
