f7eebc0b5a1bb3316a662b9935135df7e1f5fadf
WeatherBoy <dafloxi@gmail.com>
2024-12-04 17:37:34 +0100

    :wastebasket: `is_flex( )` - no longer needed

--- wtb/app/utils/string_process.py
+++ wtb/app/utils/string_process.py
@@ -8,22 +8,6 @@ def str_and_non_empty(cell: str) -> bool:
     return type(cell) is str and cell.strip() != ""
 
 
-def is_flex(function: str) -> bool:
-    """
-    Check if a function is a flex location.
-    NOTE: Sometimes the Flex function is called Flex KOOR (or something similar), and that doesn't
-    match how a flex location should be treated.
-
-    :param location: A string with the function.
-
-    :return: A boolean indicating if the function is a flex location.
-    """
-    edgecases = ["koor", "koordinator"]
-    if any(edgecase in function.lower() for edgecase in edgecases):
-        return False
-    return "flex" in function.lower()
-
-
 def strip_str(cell: str) -> str:
     """
     Strip a string of all whitespaces and make it lowercase.
