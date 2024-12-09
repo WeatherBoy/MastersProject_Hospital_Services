5759872d21c178eb82aaad6bc31c99bef9ab658e
WeatherBoy <dafloxi@gmail.com>
2024-12-09 15:32:12 +0100

    :building_construction: move global params from config.toml to __init__.py
    This was a pretty big "incursion".
    I deleted a lot of really weird code, that was supported only by global parameters having been declared in a config file.
    
    BUT DO BEWARE!!
    KJ made an excellent point, that these global parameters you now have placed in app's __init__.py, they might have to be accessed by scripts in \tests at some point. For that reason it's probably quite fragile that the \wtb project-folder doesn't have its own __init__.py

--- wtb/app/utils/os_structure.py
+++ wtb/app/utils/os_structure.py
@@ -2,6 +2,7 @@ import datetime
 
 import pandas as pd
 
+from app import NUM_WEEKDAYS
 from app.data_structures.taskboard import TaskBoard
 from app.utils.data_formatting import make_df_ready_for_visualisation
 
@@ -50,12 +51,11 @@ def save_df_to_excel(df: pd.DataFrame, writer: pd.ExcelWriter, name: str, format
             worksheet.set_column(idx, idx, max_len)  # set column width
 
 
-def save_weekly_taskboards(weekly_taskboards: list[TaskBoard], num_weekdays: int = 7, verbose: bool = True) -> None:
+def save_weekly_taskboards(weekly_taskboards: list[TaskBoard], verbose: bool = True) -> None:
     """
     Saves the weekly TaskBoards to a single Excel file. Where each sheet is a TaskBoard of the week.
 
     :param weekly_taskboards: A list of TaskBoard objects. Ordered by day of the week.
-    :param num_weekdays: (optional) The number of weekdays in the week. Default is 7.
     :param verbose: (optional) A boolean to print information about the saving process. Default is True.
     """
     today = datetime.date.today()
@@ -65,7 +65,7 @@ def save_weekly_taskboards(weekly_taskboards: list[TaskBoard], num_weekdays: int
     filename = f"data/results/XLSX/Taskboard_{year}_Week_{week}.xlsx"
 
     with pd.ExcelWriter(filename, engine="xlsxwriter") as writer:
-        for i in range(num_weekdays):
+        for i in range(NUM_WEEKDAYS):
             if weekly_taskboards[i] is None:
                 if verbose:
                     print(f"The {i + 1}th TaskBoard of the week was EMPTY and NOT saved.")
