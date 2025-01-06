2d528985d3980f60704b3e1e24e189b0675c23d2
WeatherBoy <dafloxi@gmail.com>
2025-01-06 16:06:12 +0100

    🚧 intermediate main.py CHECK_ARBEJDSPLAN

--- check_arbejdsplan/app/main.py
+++ check_arbejdsplan/app/main.py
@@ -1,6 +1,14 @@
-from app.utils.data_process import lejeplan_daily_tasks_lists, load_arbejdsplan_lejeplan
+import toml
+from app.utils.data_process import arbejdsplan_dict_with_date_keys, lejeplan_dict_with_date_keys, load_arbejdsplan_lejeplan
 
 if __name__ == "__main__":
+    # Get configs from config file
+    config = toml.load("config.toml")
+
     lejeplan, arbejdsplan = load_arbejdsplan_lejeplan("march")
-    tasks_matrix = lejeplan_daily_tasks_lists(lejeplan)
-    print(tasks_matrix)
+
+    lejeplan_dict = lejeplan_dict_with_date_keys(lejeplan)
+    arbejdsplan_dict = arbejdsplan_dict_with_date_keys(arbejdsplan, config)
+
+    print(f"LEJEPLAN:\n{lejeplan_dict}")
+    print(f"\nARBEJDSPLAN:\n{arbejdsplan_dict}")
